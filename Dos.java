8818696737d488b7c5abd5cdb061c7c051fc92fa
David-Christopher-ISIC <gaber.david.christopher@gmail.com>
2025-02-25 19:08:53 -0600

    Clase dos

---
+++ Dos.java
@@ -0,0 +1,13 @@
+public class Dos extends Uno implements Tres {
+    public void Calcular(){
+        //codigo
+    }
+    public void imprimirFactura(){
+        //codigo
+    }
+
+    @Override
+    public void calcular() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
