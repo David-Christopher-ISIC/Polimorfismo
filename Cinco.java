836e57bde3b79c9ea719998367d3aeef6aee4986
David-Christopher-ISIC <gaber.david.christopher@gmail.com>
2025-02-28 19:08:53 -0600

    Clase cinco

---
+++ Cinco.java
@@ -0,0 +1,13 @@
+public class Cinco implements Tres, Cuatro {
+
+    @Override
+    public void imprimirFactura() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void registro() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
